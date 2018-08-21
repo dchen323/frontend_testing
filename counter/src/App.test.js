b5cdb85a816ee2016cd3348fac7606388bb37f0b
Daniel <d.chen323@gmail.com>
2018-08-21 13:59:05 -0700

    Test button changes counter state

--- counter/src/App.test.js
+++ counter/src/App.test.js
@@ -9,11 +9,13 @@ Enzyme.configure({ adapter: new EnzymeAdapter() });
 /*Factory function to create a ShallowWrapper for the App component
   *@function setup
   *@param {object} props - Component props specific to this setup.
-  *@params {any} state - Initial state for setup.
+  *@params {object} state - Initial state for setup.
   *@returns {ShallowWrapper}
 */
 const setup = (props = {}, state = null) => {
-  return shallow(<App {...props} />);
+  const wrapper = shallow(<App {...props} />);
+  if (state) wrapper.setState(state);
+  return wrapper;
 };
 /*
   * Return ShallowWrapper containing node(s) with the given data-test value.
@@ -33,16 +35,32 @@ it("renders without crashing", () => {
 
 it("renders increment button", () => {
   const wrapper = setup();
-  const appComponent = findByTestAttr(wrapper, "increment-button");
-  expect(appComponent.length).toBe(1);
+  const button = findByTestAttr(wrapper, "increment-button");
+  expect(button.length).toBe(1);
 });
 
 it("renders counter display", () => {
   const wrapper = setup();
-  const appComponent = findByTestAttr(wrapper, "counter-display");
-  expect(appComponent.length).toBe(1);
+  const counterDisplay = findByTestAttr(wrapper, "counter-display");
+  expect(counterDisplay.length).toBe(1);
 });
 
-it("starts counter at 0", () => {});
+it("starts counter at 0", () => {
+  const wrapper = setup();
+  const initialCounterState = wrapper.state("counter");
+  expect(initialCounterState).toBe(0);
+});
 
-it("clicking button increments counter displays", () => {});
+it("clicking button increments counter displays", () => {
+  const counter = 7;
+  const wrapper = setup(null, { counter });
+
+  //find button and click
+  const button = findByTestAttr(wrapper, "increment-button");
+  button.simulate("click");
+  wrapper.update();
+
+  //find display and test value
+  const counterDisplay = findByTestAttr(wrapper, "counter-display");
+  expect(counterDisplay.text()).toContain(counter + 1);
+});
