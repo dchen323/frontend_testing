07dcba00e60c07bd80371da97c55af8c3d7dd409
Daniel <d.chen323@gmail.com>
2018-08-27 16:08:21 -0700

    Write code and tests for successReudcer

--- jotto/src/reducers/successReducer.js
+++ jotto/src/reducers/successReducer.js
@@ -1,5 +1,9 @@
-export default (state = null, action) => {
+import { CORRECT_GUESS } from "../actions";
+
+export default (state = false, action) => {
   switch (action.type) {
+    case CORRECT_GUESS:
+      return true;
     default:
       return state;
   }
