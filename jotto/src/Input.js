c91bc54344d85d7431463939afd8d0ba5d2dfefc
Daniel <d.chen323@gmail.com>
2018-08-27 16:45:32 -0700

    Complete Input component render tests for success: false

--- jotto/src/Input.js
+++ jotto/src/Input.js
@@ -3,14 +3,28 @@ import { connect } from "react-redux";
 
 class Input extends Component {
   render() {
-    return (
-      <div>
-        <button />
-      </div>
+    const contents = this.props.success ? null : (
+      <form className="form-inline">
+        <input
+          data-test="input-box"
+          className="mb-2 mx-sm-3"
+          id="word-guess"
+          type="text"
+          placeholder="Enter Guess"
+        />
+        <button
+          data-test="submit-button"
+          className="btn btn-primary mb-2"
+          type="submit"
+        >
+          Submit
+        </button>
+      </form>
     );
+    return <div data-test="component-input">{contents}</div>;
   }
 }
 
-const mapStateToProps = state => ({});
+const mapStateToProps = ({ success }) => ({ success });
 
 export default connect(mapStateToProps)(Input);
