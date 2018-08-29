f35b0d9866bbea0d4c040620f84b54f77baef2bc
Daniel <d.chen323@gmail.com>
2018-08-29 15:22:09 -0700

    Set up guessedword state

--- jotto/src/actions/index.js
+++ jotto/src/actions/index.js
@@ -1,3 +1,8 @@
 export const CORRECT_GUESS = "CORRECT_GUESS";
+export const GUESS_WORD = "GUESS_WORD";
 
-export const correctGuess = () => ({ type: CORRECT_GUESS });
+// export const correctGuess = () => ({ type: CORRECT_GUESS });
+
+export const guessWord = guessedWord => {
+  return function(dispatch, getState) {};
+};
--- jotto/src/reducers/index.js
+++ jotto/src/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from "redux";
 import SuccessReducer from "./successReducer";
+import guessedWords from "./guessedWords";
 
 export default combineReducers({
-  success: SuccessReducer
+  success: SuccessReducer,
+  guessedWords
 });
