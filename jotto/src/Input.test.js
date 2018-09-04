3708a20ecc345772a584f7f83c091f3a865b4c7e
Daniel <d.chen323@gmail.com>
2018-09-04 16:49:39 -0700

    Create tests and functionality for guessWord argument from input box

--- jotto/src/Input.test.js
+++ jotto/src/Input.test.js
@@ -67,19 +67,33 @@ describe("redux props", () => {
 });
 
 describe("guessWord action creator call", () => {
-  it("calls guessWord when button is clicked", () => {
-    const guessWordMock = jest.fn();
+  let guessWordMock;
+  let wrapper;
+  const guessedWord = "train";
+
+  //set up mock for guessWord
+  beforeEach(() => {
+    guessWordMock = jest.fn();
     const props = {
       guessWord: guessWordMock
     };
 
-    const wrapper = shallow(<UnconnectedInput {...props} />);
+    wrapper = shallow(<UnconnectedInput {...props} />);
 
+    //add value to input box
+    wrapper.instance().inputBox.current = { value: guessedWord };
+
+    //simulate click
     const submitButton = findByTestAttr(wrapper, "submit-button");
-    submitButton.simulate("click");
-
+    submitButton.simulate("click", { preventDefault() {} });
+  });
+  it("calls guessWord when button is clicked", () => {
     const guessWordCallCount = guessWordMock.mock.calls.length;
-
     expect(guessWordCallCount).toBe(1);
+  });
+
+  it("calls guessWord with input value as argument", () => {
+    const guessWordArg = guessWordMock.mock.calls[0][0];
+    expect(guessWordArg).toBe(guessedWord);
   });
 });
